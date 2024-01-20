cd6d641341f863b06cb7101bceb1b7d232fe418b
JangJunHo1992 <wnsghxn@naver.com>
2024-01-20 13:25:36 +0900

    파일 정리중

--- Client/Private/Loader.cpp
+++ Client/Private/Loader.cpp
@@ -498,7 +498,7 @@ HRESULT CLoader::Loading_For_Level(LEVEL eLEVEL)
 		{
 			PivotMatrix = XMMatrixScaling(0.02f, 0.02f, 0.02f) * XMMatrixRotationY(XMConvertToRadians(180.0f));
 			if (FAILED(m_pGameInstance->Add_Prototype(LEVEL::LEVEL_TOOL, strPrototypeTag,
-				CModel_Tool::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../Bin/Resources/Models/BossStage1/BossStage1", PivotMatrix))))
+				CModel_Tool::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../Bin/Resources/Models/BossStage2/BossStage2", PivotMatrix))))
 				return E_FAIL;
 		}
 		else
@@ -510,46 +510,46 @@ HRESULT CLoader::Loading_For_Level(LEVEL eLEVEL)
 				return E_FAIL;
 		}
 	}
-	//Map2
-	{
-		const wstring& strPrototypeTag = TEXT("Prototype_Component_Model_Map2");
-
-		if (LEVEL_TOOL == eLEVEL)
-		{
-// 			PivotMatrix = XMMatrixScaling(0.02f, 0.02f, 0.02f) * XMMatrixRotationY(XMConvertToRadians(180.0f));
-// 			if (FAILED(m_pGameInstance->Add_Prototype(LEVEL::LEVEL_TOOL, strPrototypeTag,
-// 				CModel_Tool::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../Bin/Resources/Models/BossStage1/BossStage1", PivotMatrix))))
-// 				return E_FAIL;
-		}
-		else
-		{
-			/* For.Prototype_Component_Model_Map2 */
-			PivotMatrix = XMMatrixScaling(0.01f, 0.01f, 0.01f) * XMMatrixRotationY(XMConvertToRadians(180.0f));
-			if (FAILED(m_pGameInstance->Add_Prototype(LEVEL::LEVEL_GAMEPLAY, strPrototypeTag,
-				CModel_GamePlay::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../Bin/Resources/Models/BossStage1/BossStage1", PivotMatrix))))
-				return E_FAIL;
-		}
-	}
-	//Map3
-	{
-		const wstring& strPrototypeTag = TEXT("Prototype_Component_Model_Map3");
-
-		if (LEVEL_TOOL == eLEVEL)
-		{
-// 			PivotMatrix = XMMatrixScaling(0.02f, 0.02f, 0.02f) * XMMatrixRotationY(XMConvertToRadians(180.0f));
-// 			if (FAILED(m_pGameInstance->Add_Prototype(LEVEL::LEVEL_TOOL, strPrototypeTag,
-// 				CModel_Tool::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../Bin/Resources/Models/BossStage2/BossStage2", PivotMatrix))))
-// 				return E_FAIL;
-		}
-		else
-		{
-			/* For.Prototype_Component_Model_Map3 */
-			PivotMatrix = XMMatrixScaling(0.02f, 0.02f, 0.02f) * XMMatrixRotationY(XMConvertToRadians(180.0f));
-			if (FAILED(m_pGameInstance->Add_Prototype(LEVEL::LEVEL_GAMEPLAY, strPrototypeTag,
-				CModel_GamePlay::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../Bin/Resources/Models/BossStage2/BossStage2", PivotMatrix))))
-				return E_FAIL;
-		}
-	}
+// 	Map2
+// 		{
+// 			const wstring& strPrototypeTag = TEXT("Prototype_Component_Model_Map2");
+// 	
+// 			if (LEVEL_TOOL == eLEVEL)
+// 			{
+// 				PivotMatrix = XMMatrixScaling(0.02f, 0.02f, 0.02f) * XMMatrixRotationY(XMConvertToRadians(180.0f));
+// 				if (FAILED(m_pGameInstance->Add_Prototype(LEVEL::LEVEL_TOOL, strPrototypeTag,
+// 					CModel_Tool::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../Bin/Resources/Models/BossStage1/BossStage1", PivotMatrix))))
+// 					return E_FAIL;
+// 			}
+// 			else
+// 			{
+// 				/* For.Prototype_Component_Model_Map2 */
+// 				PivotMatrix = XMMatrixScaling(0.01f, 0.01f, 0.01f) * XMMatrixRotationY(XMConvertToRadians(180.0f));
+// 				if (FAILED(m_pGameInstance->Add_Prototype(LEVEL::LEVEL_GAMEPLAY, strPrototypeTag,
+// 					CModel_GamePlay::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../Bin/Resources/Models/BossStage1/BossStage1", PivotMatrix))))
+// 					return E_FAIL;
+// 			}
+// 		}
+// 		//Map3
+// 		{
+// 			const wstring& strPrototypeTag = TEXT("Prototype_Component_Model_Map3");
+// 	
+// 			if (LEVEL_TOOL == eLEVEL)
+// 			{
+// 				PivotMatrix = XMMatrixScaling(0.02f, 0.02f, 0.02f) * XMMatrixRotationY(XMConvertToRadians(180.0f));
+// 				if (FAILED(m_pGameInstance->Add_Prototype(LEVEL::LEVEL_TOOL, strPrototypeTag,
+// 					CModel_Tool::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../Bin/Resources/Models/BossStage2/BossStage2", PivotMatrix))))
+// 					return E_FAIL;
+// 			}
+// 			else
+// 			{
+// 				/* For.Prototype_Component_Model_Map3 */
+// 				PivotMatrix = XMMatrixScaling(0.02f, 0.02f, 0.02f) * XMMatrixRotationY(XMConvertToRadians(180.0f));
+// 				if (FAILED(m_pGameInstance->Add_Prototype(LEVEL::LEVEL_GAMEPLAY, strPrototypeTag,
+// 					CModel_GamePlay::Create(m_pDevice, m_pContext, CModel::TYPE_NONANIM, "../Bin/Resources/Models/BossStage2/BossStage2", PivotMatrix))))
+// 					return E_FAIL;
+// 			}
+// 		}
 
 	if (LEVEL_TOOL == eLEVEL) 
 	{
@@ -629,7 +629,7 @@ HRESULT CLoader::Loading_For_Level(LEVEL eLEVEL)
 	lstrcpy(m_szLoadingText, TEXT("네비게이션를(을) 로드하는 중입니다."));
 	/* For.Prototype_Component_Navigation */
 	if (FAILED(m_pGameInstance->Add_Prototype(eLEVEL, TEXT("Prototype_Component_Navigation"),
-		CNavigation::Create(m_pDevice, m_pContext, TEXT("../Bin/DataFiles/Stage1.dat")))))
+		CNavigation::Create(m_pDevice, m_pContext, TEXT("../Bin/DataFiles/Navigation.dat")))))
 		return E_FAIL;
 
 	lstrcpy(m_szLoadingText, TEXT("콜리이더를(을) 로드하는 중입니다."));
