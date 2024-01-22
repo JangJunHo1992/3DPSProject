e53fdb41cc56a6439f2413fe982418197fb6f499
JangJunHo <wnsghxn@naver.com>
2024-01-22 14:38:16 +0900

    FPS tick 에서 안넣어줬었음

--- Client/Private/Level_GamePlay.cpp
+++ Client/Private/Level_GamePlay.cpp
@@ -187,6 +187,7 @@ HRESULT CLevel_GamePlay::Ready_Layer_Effect(const wstring& strLayerTag)
 
 	return S_OK;
 }
+
 HRESULT CLevel_GamePlay::Ready_Layer_BackGround(const wstring& strLayerTag)
 {
 // 	if (FAILED(m_pGameInstance->Add_CloneObject(LEVEL_GAMEPLAY, strLayerTag, TEXT("Prototype_GameObject_Terrain"))))
