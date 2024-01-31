973f412c30272675498abafdd3eee5647ba19ab5
JangJunHo1992 <wnsghxn@naver.com>
2024-02-01 00:31:37 +0900

    Revert "프레임 테스트 190정도"
    This reverts commit b1676cdf394338b09277dac1daf0b4e2e9d5a5d0.

--- Client/Default/Client.cpp
+++ Client/Default/Client.cpp
@@ -91,21 +91,21 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
 		fTimeAcc += pGameInstance->Compute_TimeDelta(TEXT("Timer_Default"));
 
-// 		if (fTimeAcc >= 1.f / 60.0f /* 1초에 60번만 true*/)
-// 		{
-// 			pMainApp->Tick(pGameInstance->Compute_TimeDelta(TEXT("Timer_60")));
-// 			pMainApp->Render();
-// 
-// 			fTimeAcc = 0.f;
-// 		}
-		//프레임 제한 해제
-		if (1)
+		if (fTimeAcc >= 1.f / 60.0f /* 1초에 60번만 true*/)
 		{
 			pMainApp->Tick(pGameInstance->Compute_TimeDelta(TEXT("Timer_60")));
 			pMainApp->Render();
 
 			fTimeAcc = 0.f;
 		}
+		//프레임 제한 해제
+// 		if (1)
+// 		{
+// 			pMainApp->Tick(pGameInstance->Compute_TimeDelta(TEXT("Timer_60")));
+// 			pMainApp->Render();
+// 
+// 			fTimeAcc = 0.f;
+// 		}
 	}
 
 	Safe_Release(pGameInstance);
